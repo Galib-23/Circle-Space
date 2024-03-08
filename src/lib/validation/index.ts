9f09a4ea73139090ca54f2b20d364c855c06b24c
Galib-23 <aagalib2323@gmail.com>
2024-03-08 13:18:28 +0600

    Add Post form created

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -14,3 +14,9 @@ export const signinValidation = z.object({
     password: z.string().min(6, {message: 'Password must be greater than 6 characters'}),
     
 })
+export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
+})

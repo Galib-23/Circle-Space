b663441166900bb3873e0870d0d692e76e504f5f
Galib-23 <aagalib2323@gmail.com>
2024-03-12 13:05:28 +0600

    Infinite search implemented

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -4,16 +4,24 @@ import SearchResults from "@/components/shared/SearchResults";
 import { Input } from "@/components/ui/input"
 import useDebounce from "@/hooks/useDebounce";
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations";
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer";
 
 
 const Explore = () => {
+
+  const { ref, inView } = useInView();
 
   const { data: posts, fetchNextPage, hasNextPage} = useGetPosts();
 
   const [ searchValue, setSearchValue ] = useState("");
   const debouncedValue = useDebounce(searchValue, 500);
   const { data: searchedPosts, isFetching: isSearchFetching} = useSearchPosts(debouncedValue);
+
+  useEffect(() => {
+    if(inView && !searchValue) fetchNextPage();
+  }, [inView, searchValue])
+  
 
   if(!posts) {
     return (
@@ -62,7 +70,10 @@ const Explore = () => {
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {
           shouldShowSearchResults ? (
-            <SearchResults />
+            <SearchResults 
+              isSearchFetching={isSearchFetching}
+              searchedPosts={searchedPosts}
+            />
           ) : shouldShowPosts ? (
             <p className="text-light-4 mt-10 text-center w-full">End of Posts</p>
           ) : posts.pages.map((item, index) => (
@@ -70,6 +81,13 @@ const Explore = () => {
           ))
         }
       </div>
+      {
+        hasNextPage && !searchValue && (
+          <div ref={ref} className="mt-10">
+              <Loader />
+          </div>
+        )
+      }
     </div>
   )
 }
